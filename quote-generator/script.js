05aa137e46b4630a2882c7e0ebc22fbf870ee640
Ricardo Ribeiro <r3ifes@gmail.com>
2022-04-30 04:42:19 -0300

    terminado o primeiro projeto

---
+++ quote-generator/script.js
@@ -0,0 +1,63 @@
+const quoteContainer = document.getElementById('quote-container')
+const quotetext = document.getElementById('quote')
+const authorText = document.getElementById('author')
+const twitterBtn = document.getElementById('twitter')
+const newQuoteBtn = document.getElementById('new-quote')
+const loader = document.getElementById('loader')
+
+// Show Loading
+function loading() {
+    loader.hidden = false
+    quoteContainer.hidden = true
+}
+
+// Hide Loading
+function complete() {
+    loader.hidden = true
+    quoteContainer.hidden = false
+}
+
+let apiQuotes = []
+
+// Show New Quote
+function newQuote() {
+    loading()
+    // Pick a random quote from apiQuotes array
+    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)]
+    // Check if Author field is black and replace it with "Unknown"
+    quote.author ? authorText.textContent = quote.author : authorText.textContent = 'Unknown'
+
+    quote.text.length > 80 ? quotetext.classList.add('long-quote') : quotetext.classList.remove('long-quote')
+
+    quotetext.textContent = quote.text
+    complete()
+}
+
+// Get Quotes From API
+async function getQuotes() {
+    const apiUrl = 'https://type.fit/api/quotes'
+    try {
+        const response = await fetch(apiUrl)
+        apiQuotes = await response.json()
+        newQuote()
+    } catch (error) {
+        alert(error)
+        // Catch Error Here
+    }
+}
+
+
+// Tweet Quote
+function tweetQuote() {
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quotetext.textContent} - ${authorText.textContent}`
+    window.open(twitterUrl, '_blank')
+}
+
+// Event Listeners
+newQuoteBtn.addEventListener('click', newQuote)
+twitterBtn.addEventListener('click', tweetQuote)
+
+
+// On Load
+loading()
+getQuotes()
